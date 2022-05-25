49c1e1c3ee7276b90818e9e7ee965e5844f7e492
Ollie-Judge <ollie.judge@hotmail.com>
2022-05-25 14:27:38 +0100

    added second dice roll

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,21 @@ console.log(dice1);
 dice1.setAttribute("src", randoImageSource);
 
 // second dice
+
+let randoNum2 = Math.floor(Math.random() * 6) + 1; // generates random dice number for the first dice
+
+console.log(randoNum2);
+
+let randoDiceImage2 = "dice" + randoNum2 + ".png"; //takes the images and the random number and puts it together choosing between dice1.png and dice6.png
+
+console.log(randoDiceImage2);
+
+let randoImageSource2 = "assets/img/" + randoDiceImage2; // gets the source and connects it to the random dice imageimages/dice1.png - images/dice6.png
+
+console.log(randoImageSource2);
+
+let dice2 = document.querySelectorAll("img")[1];
+
+console.log(dice2);
+
+dice2.setAttribute("src", randoImageSource2);
