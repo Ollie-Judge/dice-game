a5832782f4a76af70e616e2c049f33ae1c9294dc
Ollie-Judge <ollie.judge@hotmail.com>
2022-05-26 13:55:45 +0100

    removed console logs as they arent needed anymore

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,11 @@
 
 let randoNum1 = Math.floor(Math.random() * 6) + 1; // generates random dice number for the first dice
 
-console.log(randoNum1);
-
 let randoDiceImage = "dice" + randoNum1 + ".png"; //takes the images and the random number and puts it together choosing between dice1.png and dice6.png
-
-console.log(randoDiceImage);
 
 let randoImageSource = "assets/img/" + randoDiceImage; // gets the source and connects it to the random dice imageimages/dice1.png - images/dice6.png
 
-console.log(randoImageSource);
-
 let dice1 = document.querySelectorAll("img")[0];
-
-console.log(dice1);
 
 dice1.setAttribute("src", randoImageSource);
 
@@ -22,19 +14,11 @@ dice1.setAttribute("src", randoImageSource);
 
 let randoNum2 = Math.floor(Math.random() * 6) + 1; // generates random dice number for the first dice
 
-console.log(randoNum2);
-
 let randoDiceImage2 = "dice" + randoNum2 + ".png"; //takes the images and the random number and puts it together choosing between dice1.png and dice6.png
-
-console.log(randoDiceImage2);
 
 let randoImageSource2 = "assets/img/" + randoDiceImage2; // gets the source and connects it to the random dice imageimages/dice1.png - images/dice6.png
 
-console.log(randoImageSource2);
-
 let dice2 = document.querySelectorAll("img")[1];
-
-console.log(dice2);
 
 dice2.setAttribute("src", randoImageSource2);
 
